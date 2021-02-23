92e33b0bd3657bfbc8300f19627a14643be5d2f4
Johnny Horvi <johnny@horvi.no>
2021-02-23 15:09:10 +0100

    metrics works

--- src/main/kotlin/io/nais/aivvia/Application.kt
+++ src/main/kotlin/io/nais/aivvia/Application.kt
@@ -1,9 +1,19 @@
 package io.nais.aivvia
 
 import io.ktor.application.*
+import io.ktor.metrics.micrometer.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.netty.EngineMain
+import io.micrometer.core.instrument.Clock
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
+import io.prometheus.client.CollectorRegistry
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
@@ -11,7 +21,21 @@ fun main(args: Array<String>) {
 
 @Suppress("unused")
 fun Application.module() {
-   install(Routing){
-       nais()
-   }
+    install(Routing) {
+        nais()
+    }
+    install(MicrometerMetrics) {
+        registry = PrometheusMeterRegistry(
+                PrometheusConfig.DEFAULT,
+                CollectorRegistry.defaultRegistry,
+                Clock.SYSTEM
+        )
+        meterBinders = listOf(
+                ClassLoaderMetrics(),
+                JvmMemoryMetrics(),
+                JvmGcMetrics(),
+                ProcessorMetrics(),
+                JvmThreadMetrics()
+        )
+    }
 }
