5f6b59823b9a07adb01c55b2418262b12351a1fe
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-02-23 09:40:04 +0100

    Initial Dockerfile and use jdk 15

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_15
+    targetCompatibility = JavaVersion.VERSION_15
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
